fda649308484cf143068750ee3d0091108189a3c
AfaaqRamzan <69888091+AfaaqRamzan@users.noreply.github.com>
2024-09-06 00:44:20 +0500

    Build & Test: DELETE API

--- backend/server.js
+++ backend/server.js
@@ -23,10 +23,20 @@ app.post("/api/products", async (req, res) => {
 
   try {
     await newProduct.save();
-    res.status(201).json({ success: false, data: newProduct });
+    res.status(201).json({ success: true, data: newProduct });
   } catch (error) {
     console.log("Error in Create product:", error.message);
     res.status(500).json({ success: false, message: "Server Error" });
+  }
+});
+
+app.delete("/api/products/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    await Product.findByIdAndDelete(id);
+    res.status(200).json({ success: true, message: "Product deleted" });
+  } catch (error) {
+    res.status(404).json({ success: false, message: "Product not found" });
   }
 });
 
