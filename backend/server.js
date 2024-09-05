6bd9ec181a757be835e2c5d9c836e83bc8e710a4
AfaaqRamzan <69888091+AfaaqRamzan@users.noreply.github.com>
2024-09-06 00:24:43 +0500

    Build & Test First API

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,33 @@
 import express from "express";
 import dotenv from "dotenv";
 import { conntecDB } from "./config/db.js";
+import Product from "./models/product.model.js";
 
 dotenv.config();
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Server is ready");
+//middle ware that allow us to accept JSON data in the req.body
+app.use(express.json());
+
+app.post("/api/products", async (req, res) => {
+  const product = req.body; // user will send this data
+
+  if (!product.name || !product.price || !product.image) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Please provide all fields" });
+  }
+
+  const newProduct = new Product(product);
+
+  try {
+    await newProduct.save();
+    res.status(201).json({ success: false, data: newProduct });
+  } catch (error) {
+    console.log("Error in Create product:", error.message);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
 });
 
 app.listen(5000, () => {
